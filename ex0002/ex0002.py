0cf17c3d979ba36c9cf154adc163dedf91991cd4
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-11-07 01:07:45 -0300

    Exercícios em python 01 a 05

---
+++ ex0002/ex0002.py
@@ -0,0 +1,6 @@
+'''Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.'''
+
+nome = str(input('Qual seu nome?'))
+sexo = str(input('Qual seu sexo? F/M'))
+
+print('Seja bem vinda', nome)
