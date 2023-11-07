886d1a0d7021660003370f3458a863f2a9df0cc9
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-11-07 03:25:20 -0300

    Update ex0006.py

--- ex0006/ex0006.py
+++ ex0006/ex0006.py
@@ -4,4 +4,4 @@ num = int(input('Digite um número: '))
 dobro = num * 2
 triplo = num * 3
 raiz = num ** (1/2)
-print('O dobro de {} é {} \nO triplo de {} é {} \nA raiz quadrada de {} é {}'.format(num, dobro, num, triplo, num, raiz))
+print('O dobro de {} é {} \nO triplo de {} é {} \nA raiz quadrada de {} é {:.2f}'.format(num, dobro, num, triplo, num, raiz))
