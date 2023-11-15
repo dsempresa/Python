5e915244dbb2868192225f4e1226453cae89afcb
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-11-15 00:24:37 -0300

    exercícios 28 e 29 concluídos

--- ex0028/ex0028.py
+++ ex0028/ex0028.py
@@ -2,9 +2,14 @@
 Escreva um programa que faça o computador pensar em um número inteiro de 0 a 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. 
 O programa deverá escrever na tela se o usuário venceu ou perdeu.
 '''
-
+import time
 import random 
-numero = int(input('Digite um número inteiro de 0 a 5: '))
-
-print('número escolhido pelo computador foi...{}'.format())
+numero = int(input('qual número o computador está pensando? '))
+time.sleep(2)
+pc = int(random.randint(0, 5))
+print('número escolhido pelo computador foi...{}'.format(pc))
+if numero == pc:
+    print('Parabens você acertou')
+else:
+    print('Vc errou!\nTente outra vez!')
 
