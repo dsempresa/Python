cf294d5ba499cdd4179b40e5529cf8649a097a98
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-11-14 23:30:13 -0300

    Update ex0028.py

--- ex0028/ex0028.py
+++ ex0028/ex0028.py
@@ -2,3 +2,9 @@
 Escreva um programa que faça o computador pensar em um número inteiro de 0 a 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. 
 O programa deverá escrever na tela se o usuário venceu ou perdeu.
 '''
+
+import random 
+numero = int(input('Digite um número inteiro de 0 a 5: '))
+
+print('número escolhido pelo computador foi...{}'.format())
+
