2317233e71e53226d69337c97aa667d357df8c0a
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-11-29 02:39:39 -0300

    exercícios 43, 44 e 45

--- ex0043/ex0043.py
+++ ex0043/ex0043.py
@@ -5,3 +5,18 @@
 # -25 até 30:Sobrepeso
 # -30 até 40:Obesidade
 # -Acima de 40:Obesidade mórbida
+
+altura = float(input('Digite sua altura: '))
+peso = float(input('Digite seu peso: '))
+imc = peso / (altura * altura)
+
+if imc < 18.5:
+    print('Seu IMC é {},Você está abaixo do peso ideal.'.format(imc))
+elif imc >= 18.5 and imc < 25:
+    print('Seu IMC é {},Você está com peso ideal.'.format(imc))
+elif imc >= 25 and imc < 30:
+    print('Seu IMC é {},Você está sobrepeso.'.format(imc))
+elif imc >= 30 and imc <= 40:
+    print('Seu IMC é {},Você está obeso.'.format(imc))
+else:
+    print('Seu IMC é {},Você está em obesidade mórbida.'.format(imc))
--- ex0044/ex0044.py
+++ ex0044/ex0044.py
@@ -1,6 +1,21 @@
 # Elabore um programa que calcule o valor a ser pago por um produto,considerando o seu preço normal e condição de pagamento:
 
-# -A vista dinheiro/cheque:1o% de desconto
-# -A viata no cartão:5% de desconto
+# -A vista dinheiro/cheque:10% de desconto
+# -A vista no cartão:5% de desconto
 # -Em ate 2x no cartão:preço normal
 # -Em 3x ou mais no cartão:20% de juros
+
+precoProduto = float(input('Digite o valor do produto: R$'))
+formaPagamento = int(input('Digite a forma de pagamento:\n[ 1 ] Dinheiro ou Cheque\n[ 2 ] Avista no cartão\n[ 3 ] Em até 2x vezes no cartão\n[ 4 ] Em até 3x no cartão\n'))
+
+if formaPagamento == 1:
+    print('Com pagamento em dinheiro ou cheque você terá desconto de 10%,com isso valor do produto ficará em R${:.2f}'.format(precoProduto - (10 * precoProduto) / 100))
+elif formaPagamento == 2:
+    print('Com pagamento a vista no cartão você terá desconto de 5%,com isso valor do produto ficará em R${:.2f}'.format(precoProduto - (5 * precoProduto) / 100))
+elif formaPagamento == 3:
+    print('Com pagamento em 2x no cartão você pagará o valor normal do produto que é R${:.2f}'.format(precoProduto))
+elif formaPagamento == 4:
+    print('Com pagamento em 3x no cartão você pagará um juros de 20% em cima do valor do produto,por tanto o novo valor será R${:.2f}'.format(precoProduto + (20 * precoProduto) / 100))
+else:
+    print('Opção inválida,tente novamente.')
+
