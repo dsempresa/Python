43b81acca4b74f50bd9c3037bd4d6bad0d34fd49
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-11-30 02:05:44 -0300

    Update ex0046.py

---
+++ ex0046/ex0046.py
@@ -0,0 +1,11 @@
+# # for c in range(1,10):
+# #     print('Oi')
+# # print('Fim')
+# s = 0
+# for c in range(0,3):
+#     n = int(input('Digite um número: '))
+#     s += n
+# print('O somatório dos números é {}'.format(s))
+
+
+# Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artificios,indo de 10 até 0,com uma pausa de 1 segundos entre eles.
