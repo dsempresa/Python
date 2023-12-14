035edf2f39c4afcec894474419a9f105a58d54cc
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-12-13 23:54:21 -0300

    exercício 56 - analisador de dados

--- ex0056/ex0056.py
+++ ex0056/ex0056.py
@@ -1,4 +1,28 @@
 # Desenvolva um programa que leia o nome,idade e sexo de 4 pessoas. No final do programa,mostre:
 # *** A media de idade do grupo
-# *** Qual é o nomedo homem mais velho
+# *** Qual é o nome do homem mais velho
 # *** Quantas mulheres tem menos de 20 anos
+somaIdade = 0
+mediaIdade = 0
+maiorIdadeHomem = 0
+nomeVelho = ''
+totalMulher20 =0
+for pessoas in range(1,5):
+    print('------- {}ª PESSOA ---------'.format(pessoas))
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo: [M/F]: ')).strip()
+    somaIdade += idade
+    if pessoas == 1 and sexo in 'Mm':
+        maiorIdadeHomem = idade
+        nomeVelho = nome
+    if sexo in 'Mm' and idade > maiorIdadeHomem:
+        maiorIdadeHomem = idade
+        nomeVelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totalMulher20 += 1
+mediaIdade = somaIdade / 4
+print('A média de idade do grupo é de {} anos'.format(mediaIdade))
+print('O homem mais velho tem {} anos e se chama {}'.format(maiorIdadeHomem,nomeVelho))
+print('Ao todo são {} mulheres com menos de 20 anos'.format(totalMulher20))
+
