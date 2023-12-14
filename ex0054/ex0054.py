9412c5eeb31457cb4b63f940c1367cd4b6f0da3e
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-12-13 22:55:54 -0300

    exercício 54 e 55

--- ex0054/ex0054.py
+++ ex0054/ex0054.py
@@ -1,1 +1,14 @@
 # Crie um programa que leia o ano de nascimento de sete pessoas. No final,mostre quantas pessoas ainda não atingiram a maioridade a quantas ja são maiores. 
+from datetime import date
+atual = date.today().year
+totalmaior = 0
+totalmenor = 0
+for pessoas in range(1,8):
+    nasc = int(input('Em que ano a {}ª pessoas nasceu? '.format(pessoas)))
+    idade = atual - nasc
+    if idade >= 21:
+        totalmaior += 1
+    else:
+        totalmenor += 1
+print('Ao todo tivemos {} pessoas maiores de idade'.format(totalmaior))
+print('E tambem tivemos {} pessoas menores de idade.'.format(totalmenor))
--- ex0055/ex0055.py
+++ ex0055/ex0055.py
@@ -1,1 +1,5 @@
 # Faça um programa que leia a peso de cinco pessoas. No final,mostre qual foi o maior e o menor peso lidos.
+
+for pessoas in range(1,6):
+    peso = float(input('Peso da {}ª pessoa: '.format(pessoas)))
+    
