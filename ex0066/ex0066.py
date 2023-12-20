e91750d5c4d705794b27a25049fed35f507a08b8
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-12-20 02:57:06 -0300

    ex 66 e 67 concluidos

--- ex0066/ex0066.py
+++ ex0066/ex0066.py
@@ -1,11 +1,11 @@
 # Crie um programa que leia varios numeros inteiros pelo teclado. O programa so vai parar quando o usuario digitar o valor 999,que é a condição de parada. No final mostre quantos numeros foram digitados e qual foi a soma entre eles(desconsiderando flag).
 
-n = soma = contador = 0
+soma = contador = 0
 while True:
     n = int(input('Digite um numero(digite 999 para parar): '))
     if n == 999:
         break
     contador += 1
-    soma = soma + n
+    soma += n
 print(f'Foram somados {contador} números, e a soma entre eles é {soma}')
 
--- ex0067/ex0067.py
+++ ex0067/ex0067.py
@@ -1,1 +1,39 @@
 # Faça um programa que mostre a tabuada de varios numeros,um de cada vez,para cada valor digitado pelo usuario. O programa será interrompido quando o numero solicitado for negativo.
+# print('='*20,'PROGRAMA TABUADA','='*20)
+# num = int(input('Quer vê a tabuada de qual valor? '))
+# print('='*20,'PROGRAMA TABUADA','='*20)
+# num = int(input('Quer vê a tabuada de qual valor? '))
+# contador = 0
+# while contador < 10:
+#     while True:
+#         # while contador < 10:
+        
+#         tabuada = print(f'{num} x {contador} = {num*contador}')
+#         contador += 1
+#         if contador == 10:
+#             while True:
+#                 print('='*20,'PROGRAMA TABUADA','='*20)
+#                 num = int(input('Quer vê a tabuada de qual valor? '))
+#     # if num < 0:
+#     #     break
+   
+    
+    
+#         # if contador == 10:
+#         #     break
+# print('Programa de tabuada encerrada. Volte sempre!!!!!!')
+while True: 
+    
+    num = int(input('Quer le a tabuada de qual valor?'))
+    if num < 0:
+        break
+    print('='*20,'PROGRAMA TABUADA','='*20)
+    for c in range(1,11):
+        print(f'{num} x {c} = {num*c}')
+    print('='*20,'PROGRAMA TABUADA','='*20)
+print('Programa de tabuada encerrada!')
+   
+    
+
+
+    
