67b9806d1a762203441f8776e05ec46ad71c42c0
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-12-28 02:28:06 -0300

    exercícios 69 e 70

--- ex0069/ex0069.py
+++ ex0069/ex0069.py
@@ -2,3 +2,29 @@
 #QUANTAS PESSOAS TEM MAIS DE 18 ANOS
 #QUANTOS HOMENS FORAM CADASTRADOS
 #QUANTAS MULHERES TEM MAIS DE 20 ANOS.
+
+print('-'*30)
+print('CADASTRE UMA PESSOA')
+print('-'*30)
+tot18 = totH = totM20 = 0
+while True:
+    idade = int(input('idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F ]')).strip().upper()[0]
+    if idade >= 18:
+        tot18 += 1
+    if sexo == 'M':
+        totH += 1
+    if sexo == 'F' and idade <= 20:
+        totM20 += 1
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar?  [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'Pessoas com mais de 18 anos: {tot18} ')
+print(f'Ao todo temos {totH} homens cadastrados. ')
+print(f'Temos {totM20} mulheres com menos de 20 anos de idade. ')
+
+
--- ex0070/ex0070.py
+++ ex0070/ex0070.py
@@ -2,3 +2,26 @@
 #QUAL O TOTAL GASTO NA COMPRA.
 #QUANTOS PRODUTOS CUSTAM MAIS DE R$1000,00
 #QUAL O NOME DO PRODUTO MAIS BARATO
+totGasto = totMil = menor = contador = 0
+barato = ''
+while True:
+    produto = str(input('Qual produto você comprou? '))
+    preco = float(input(f'Qual preço do {produto}?'))
+    contador += 1
+    totGasto += preco
+    if preco > 1000:
+        totMil += 1
+    if contador == 1 or preco < menor:
+        menor = preco
+        barato = produto
+    contCompra = ' '
+    while contCompra not in 'SN':
+        contCompra = str(input('Vai continuar comprando aqui? ')).strip().upper()[0]    
+    if contCompra == 'N':
+        break
+print('Acabou')
+print(f'voce gastou um total de R${totGasto:.2f}')
+print(f'Temos {totMil} prdutos custando mais de R$1000,00')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
+
+
