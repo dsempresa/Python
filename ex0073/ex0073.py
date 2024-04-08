55b2ea830d901323f8a5ccc4d6ea7f8000a4bdef
dsempresa <130731804+dsempresa@users.noreply.github.com>
2024-04-08 20:25:02 -0300

    inicio de exercicios de tupla

---
+++ ex0073/ex0073.py
@@ -0,0 +1,14 @@
+# lanche  = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata frita')
+# for cont in range(0, len(lanche)):
+#     print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+
+# for pos, comida in enumerate(lanche):
+#     print(f'Eu vou comer {comida} na posição {pos}')
+# print('Comi pra caramba!!!!')
+
+# Crie uma tupla preenchida com os 20 primeiros colocados da tabela do brasileirão,na ordem de colocação. Depois mostre:
+# A- Apenas os 5 primeiros colocados
+# B- Os ultimos 4 colocados
+# C- Uma lista com times em ordem alfabética
+# D- Em que posição na tabela está o time do Fortaleza.
---
+++ ex0077/ex0077.py
@@ -0,0 +1,1 @@
+# Crie um programa que tenha uma tupla com varias palavras(não usar acentos).Depois disso,voce deve mostrar,para cada palavra,quais são as suas vogais.
