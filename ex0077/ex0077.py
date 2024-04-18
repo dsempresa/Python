dbe04162cbe32b70f0f4f8c9996efc0ca0ee354d
dsempresa <130731804+dsempresa@users.noreply.github.com>
2024-04-18 19:25:45 -0300

    Update ex0077.py

--- ex0077/ex0077.py
+++ ex0077/ex0077.py
@@ -1,1 +1,12 @@
 # Crie um programa que tenha uma tupla com varias palavras(não usar acentos).Depois disso,voce deve mostrar,para cada palavra,quais são as suas vogais.
+
+palavras = ('aprender', 'programar', 'linguagem', 'python',
+            'curso', 'gratis', 'estudar', 'praticar',
+            'trabalhar', 'mercado', 'programador', 'futuro')
+
+for p in palavras:
+    print(f'\nNa palav ra {p.upper()} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
+            
