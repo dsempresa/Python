4df299a508475606e46a12bfea92e76f79ba9916
dsempresa <130731804+dsempresa@users.noreply.github.com>
2024-04-19 01:10:17 -0300

    exercicios de listas

--- ex0082/ex0082.py
+++ ex0082/ex0082.py
@@ -3,3 +3,19 @@
 # Depois disso,crie duas listas extras que vão conter apenas os valores pares e os valores impares digitados,respectivamente.
 
 # Ao final, mostre o conteudo das tres listas geradas.
+numeros = []
+pares = []
+impares = []
+while True:
+    n = int(input('Digite um valor: '))
+    numeros.append(n)
+    if n % 2 == 0:
+        pares.append(n)
+    else:
+        impares.append(n)
+    r = str(input('Quer continuar? [S/N] '))
+    if r in 'Nn':
+        break
+print(f'A lista completa de numeros é {numeros}.')
+print(f'A lista de pares é {pares}.')
+print(f'A lista de ímpares é {impares}.')
--- ex0083/ex0083.py
+++ ex0083/ex0083.py
@@ -1,1 +1,18 @@
 # Crie um programa onde o usuario digite uma expressão qualquer que ue parenteses. Seu aplicativo deverá analisar se a expressão passada está com os parenteses abertos e fechados na ordem correta.
+
+expressao = str(input('Digite a expressão: '))
+pilha = []
+for simbolo in expressao:
+    if simbolo == '(':
+        pilha.append('(')
+    elif simbolo == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão não é válida!!')
+    
