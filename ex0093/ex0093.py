7ad68f4494e3db11d7c33fb2493f962f7e692a10
dsempresa <130731804+dsempresa@users.noreply.github.com>
2024-05-25 01:36:13 -0300

    Update ex0093.py

--- ex0093/ex0093.py
+++ ex0093/ex0093.py
@@ -5,5 +5,17 @@ partidas = []
 jogador['nome'] = str(input('Nome do jogador: '))
 totPartidas= int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
 for c in range(0, totPartidas):
-    partidas.append()
+    partidas.append(int(input(f'   Quantos gols na partida {c+1}?')))
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+print('-='*30)
 print(jogador)
+print('-='*30)
+for chave, valores in jogador.items():
+    print(f' O campo {chave} tem o valor {valores}')
+print('-='*30)
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas')
+for i, v in enumerate(jogador['gols']):
+    print(f'     => Na partida {i+1}, fez {v} gols.')
+print(f'Foi um total de {jogador["total"]} gols.')
+
